9ebee1cf8a9b0ef07f5ceb49bb2e27a43c05db60
Maurício Cunha <mauricio.nascimento.cunha@gmail.com>
2023-11-24 17:40:38 -0300

    SpaceSpectacle atualização

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar
 from .views import imagem
 #Vamos primeiro criar uma lista
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path("buscar", buscar, name="buscar"),
 ]
 

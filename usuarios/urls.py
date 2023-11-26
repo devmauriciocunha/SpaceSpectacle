7b9b9af7b744f2f614c7bd4f26649a8f1d1851fc
Maurício Cunha <mauricio.nascimento.cunha@gmail.com>
2023-11-26 20:00:41 -0300

    projeto SpaceSpectacle

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from usuarios.views import login, cadastro
+from usuarios.views import login, cadastro, logout
 
 
 
 urlpatterns = [
     path('login', login, name='login'),
     path('cadastro', cadastro, name='cadastro'),
+    path('logout', logout, name='logout'),
     
     
 

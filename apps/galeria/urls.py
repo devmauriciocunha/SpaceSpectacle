4b6c7ef488449efda2a04274999e3e6e66992080
Maurício Cunha <mauricio.nascimento.cunha@gmail.com>
2023-12-02 00:51:09 -0300

    atualização

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from apps.galeria.views import \
     index, imagem, buscar, nova_imagem, editar_imagem, deletar_imagem
+ 
+from . import views   
+    
 
 #Vamos primeiro criar uma lista
 
@@ -9,8 +12,8 @@ urlpatterns = [
     path('imagem/<int:foto_id>', imagem, name='imagem'),
     path("buscar", buscar, name="buscar"),
     path("nova-imagem", nova_imagem, name="nova_imagem"),
-    path("editar-imagem", editar_imagem, name="editar_imagem"),
-    path("deletar-imagem", deletar_imagem, name="deletar_imagem"),
+    path('editar-imagem/<int:foto_id>/', views.editar_imagem, name='editar_imagem'),
+    path("deletar-imagem/<int:foto_id>/", views.deletar_imagem, name="deletar_imagem"),
 ]
 
 

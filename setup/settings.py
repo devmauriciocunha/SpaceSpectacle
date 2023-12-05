890e803cece8b7dee1e43b0c297b44585833944e
Maurício Cunha <72228542+devmauriciocunha@users.noreply.github.com>
2023-12-05 10:53:30 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["spacespectacle-dev-jnsp.2.us-1.fl0.io", "localhost"]
 
 
 # Application definition
